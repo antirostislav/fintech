c5506121207b37c6bfdb0a4124275eec42680b84
Daniil Samoilov <dansamoru@ya.ru>
2021-11-15 22:15:41 +0300

    Инициализация микросервисов

--- fintech/urls.py
+++ fintech/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('authentication', include('authentication.views')),
+    path('finance', include('finance.views')),
+    path('recommendations', include('recommendations.views'))
 ]
