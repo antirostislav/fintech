6984a6b1fa2e6dfc85f81318a27ba94e8c8e984d
Daniil Samoilov <dansamoru@ya.ru>
2021-11-15 22:37:48 +0300

    Инициализация базы данных

--- fintech/settings.py
+++ fintech/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'fintech.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'fintech',
+        'USER': 'djangouser',
+        'PASSWORD': 'PasswordForDjangoUser288',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
