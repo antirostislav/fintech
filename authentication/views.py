d2bbb98c0c6acbf13066df98300346213a61f0f2
Daniil Samoilov <dansamoru@ya.ru>
2021-12-27 17:27:48 +0300

    Добавление функционала выхода из аккаунта

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponseBadRequest
 from django.shortcuts import render
-from .backends import login, register
+from .backends import login, register, logout
 from .forms import RegisterForm, LoginForm
 
 
@@ -10,10 +10,15 @@ def login_page(request):
         if form.is_valid():
             form = login(request, form)
             if not form.errors:
-                return HttpResponseRedirect('/home')
+                if not form.errors:
+                    if request.GET.__contains__('next'):
+                        get_params = request.GET.copy()
+                        return HttpResponseRedirect(get_params.pop('next')[0] + '?' + get_params.urlencode())
+                    else:
+                        return HttpResponseRedirect('/home?' + request.GET.urlencode())
     else:
         form = LoginForm()
-    return render(request, 'authentication.html', {'form': form})
+    return render(request, 'authentication/authentication.html', {'form': form})
 
 
 def register_page(request):
@@ -22,7 +27,16 @@ def register_page(request):
         if form.is_valid():
             form = register(form)
             if not form.errors:
-                return HttpResponseRedirect('/authentication/login')
+                return HttpResponseRedirect('/authentication/login?' + request.GET.urlencode())
     else:
         form = RegisterForm()
-    return render(request, 'authentication.html', {'form': form})
+    return render(request, 'authentication/authentication.html', {'form': form})
+
+
+def logout_page(request):
+    if request.method == 'POST':
+        return HttpResponseBadRequest()
+    else:
+        logout(request)
+        return HttpResponseRedirect('/')
+
